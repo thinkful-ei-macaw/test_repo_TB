791dc35718cb6886bf1cea48b0c4a18b7da7ee7e
Trina <trinamceary@gmail.com>
2020-02-10 21:31:33 -0800

    tic tac toe!

--- drills2.js
+++ drills2.js
@@ -51,3 +51,75 @@ function decode(sentence) {
   console.log(letters.toString());
 }
 decode('craft block argon meter bells brown croon droop');
+
+function daysInAMonth(month, leapYear){
+  switch (month) {
+  case 'January':
+    console.log('Janurary has 31 days');
+    break;
+  case 'Feburary':
+    console.log('Feburary has 28 days');
+    break;
+    //How do I add leapYear condition!?
+  case leapYear === true: 
+    console.log('February has 29 days');
+    break;
+  case 'March':
+    console.log('March has 31 days');
+    break;
+  case 'April':
+    console.log('April has 30 days');
+    break;
+  case 'May':
+    console.log('May has 31 days');
+    break;
+  case 'June':
+    console.log('June has 30 days');
+    break;
+  case 'July':
+    console.log('July has 31 days');
+    break;
+  case 'August':
+    console.log('August has 31 days');
+    break;
+  case 'September':
+    console.log('September has 30 days');
+    break;
+  case 'October':
+    console.log('October has 31 days');
+    break;
+  case 'November':
+    console.log('November has 30 days');
+    break;
+  case 'December':
+    console.log('December has 31 days');
+    break;
+  default:
+    //Not sure how to properly write error message!
+    throw new Error ('Must provide a valid month');
+  }
+}
+daysInAMonth('April');
+
+// 1 = rock, 2 = paper, 3 = scissors
+function game (num){
+  const randomNo = Math.floor(Math.random() * 3) + 1;
+  console.log(`I chose ${randomNo}`);
+  console.log(`You chose ${num}`);
+  try {
+    (num < 1) || (num > 3);
+  }
+  catch(error){
+    console.error('Number must be between 1 and three');
+  }
+  if (randomNo === num){
+    console.log('Its a tie!');
+  }
+  else if (((randomNo === 1) && (num === 3)) || ((randomNo === 2) && (num === 1)) || ((randomNo === 3) && (num === 2))) {
+    console.log('I win!');
+  }
+  else {
+    console.log('You win!');
+  }
+}
+game(3);
