1b7da24df1a9acd2aaf09ef06931a393a0850c5a
Trina <trinamceary@gmail.com>
2020-02-10 18:15:37 -0800

    add yearOfBirthFunction

--- drills1.js
+++ drills1.js
@@ -1,9 +1,16 @@
 'use strict';
-console.log('Hi, my name is Brandon and I am 27 years old');
-
+//console.log('Hi, my name is Brandon and I am 27 years old');
+function getYearOfBirth(age){
+  const YOB = 2020 - age;
+  return YOB;
+}
 function createGreeting(name, age) {
   return `Hello, my name is ${name} and I'm ${age}!`;
 }
+const greeting1 = createGreeting();
+console.log(greeting1);
+
+
 
 let studentName1 = 'Brandon';
 let studentName2 = 'Trina';
