ab49b4005c321a1e11fcfeeee7163b3c801f6e3b
bleek42 <brandonleek42@gmail.com>
2020-02-10 20:11:14 -0500

    5th update

--- test.js
+++ test.js
@@ -1,3 +1,7 @@
+function getYearOfBirth(age){
+    return 2020 - age;
+}
+
 function createGreeting(name, age){
     const YOB = 2020 - age;
     return `Hi, my name is ${name} and I was born in ${age}`;
