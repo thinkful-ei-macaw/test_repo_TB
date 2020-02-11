ac348e3cc459095f20e140dde7938ab772f95793
Trina <trinamceary@gmail.com>
2020-02-10 20:17:08 -0800

    decode function

--- drills2.js
+++ drills2.js
@@ -25,3 +25,29 @@ function beyond(num) {
   return;
 }
 beyond(0);
+
+function decode(sentence) {
+  let words = sentence.split(' ');
+  let letters = [];
+  for (let i = 0; i < words.length; i++){
+    if (words[i].charAt(0) === 'a'){
+      letters.push(words[i].charAt(1));
+    }
+    else if (words[i].charAt(0) === 'b'){
+      letters.push(words[i].charAt(2));
+    }
+    else if (words[i].charAt(0) === 'c'){
+      letters.push(words[i].charAt(3));
+    }
+    else if (words[i].charAt(0) === 'd'){
+      letters.push(words[i].charAt(4));
+    }
+    else {
+      letters.push(' ');
+    }
+  }
+  console.log(words);
+  console.log(letters);
+  console.log(letters.toString());
+}
+decode('craft block argon meter bells brown croon droop');
