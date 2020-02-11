00e164731f56e283f464f0d1a591271770465195
bleek42 <brandonleek42@gmail.com>
2020-02-10 19:57:01 -0500

    5th update

--- test.js
+++ test.js
@@ -1,13 +1,6 @@
 console.log('Hi, my name is Brandon and I'm 27 years old');
 
 function createGreeting(name, age) {
-    return `Hello, my name is ${name} and I'm ${age}!`;
+    const yearOfBirth = 2020 - age;
+    return `My name is ${name} and I was born in ${yearOfBirth}`;
 }
-
-let studentName1 = 'Brandon';
-let studentName2 = 'Trina';
-let studentAge1 = 27;
-let studentAge2 = 25;
-
-createGreeting(studentName1, studentAge1);
-createGreeting(studentName2, studentAge2);
