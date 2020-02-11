58c93c7e4d217ebb07efea4af0ef71bacf8310df
bleek42 <brandonleek42@gmail.com>
2020-02-10 19:46:28 -0500

    4th update

--- test.js
+++ test.js
@@ -1,8 +1,13 @@
 console.log('Hi, my name is Brandon and I'm 27 years old');
 
-function createGreeting() {
-    return 'Brandon and Trina want to sell hello world!';
+function createGreeting(name, age) {
+    return `Hello, my name is ${name} and I'm ${age}!`;
 }
 
-const greeting1 = createGreeting();
-console.log(greeting1);
+let studentName1 = 'Brandon';
+let studentName2 = 'Trina';
+let studentAge1 = 27;
+let studentAge2 = 25;
+
+createGreeting(studentName1, studentAge1);
+createGreeting(studentName2, studentAge2);
