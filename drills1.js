d98c39351aa31b9c08e6214075060663a5681eac
Trina <trinamceary@gmail.com>
2020-02-11 09:40:55 -0800

    shortened  variables

--- drills1.js
+++ drills1.js
@@ -1,21 +1,20 @@
+/* eslint-disable no-console */
 'use strict';
 //console.log('Hi, my name is Brandon and I am 27 years old');
 function getYearOfBirth(age){
   const YOB = 2020 - age;
   return YOB;
 }
+let name1 = 'Brandon';
+let name2 = 'Trina';
+let age1 = 27;
+let age2 = 25;
+
 function createGreeting(name, age) {
-  return `Hello, my name is ${name} and I'm ${age}!`;
+  return `Hello, my name is ${name} and I'm ${age}, I was born in ${getYearOfBirth()}!`;
 }
 const greeting1 = createGreeting();
 console.log(greeting1);
 
-
-
-let studentName1 = 'Brandon';
-let studentName2 = 'Trina';
-let studentAge1 = 27;
-let studentAge2 = 25;
-
-console.log(createGreeting(studentName1, studentAge1));
-console.log(createGreeting(studentName2, studentAge2));
+console.log(createGreeting(name1, age1));
+console.log(createGreeting(name2, age2));
